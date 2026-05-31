19aed44fa5f5a3c2f112b46223bd50f53968bf93
Mário Fernando Gomes Neto <mariosoares20012005@gmail.com>
2026-05-31 15:07:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const MarkMass = 78;
+// const MarkHeight = 1.69;
+
+// const JhonMass = 92;
+// const JhonHwight = 1.95;
+
+// const MarkBMI = MarkMass / MarkHeight ** 2;
+// const JhonBMI = JhonMass / JhonHwight ** 2;
+
+// const BMI =
+//   MarkBMI > JhonBMI
+//     ? `Mark ${MarkBMI} has a higher BMI than John ${JhonBMI}`
+//     : `Jhon ${JhonBMI} has a higher BMI than Mark ${MarkBMI}`;
+
+// console.log(BMI);
+
+
+
