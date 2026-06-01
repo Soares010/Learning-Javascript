75b365b0f55b3362ca21c121603c45f2923c6583
Mário Fernando Gomes Neto <mariosoares20012005@gmail.com>
2026-06-01 20:57:37 +0200

    A new exercise solved

--- script.js
+++ script.js
@@ -14,5 +14,17 @@
 
 // console.log(BMI);
 
+// const koalas = (97 + 112 + 101) / 3;
+// const dolphins = (109 + 95 + 123) / 3;
+// const score = 100;
 
+// const calcAverage =
+//   koalas > dolphins && koalas >= score
+//     ? `Koalas wins dolphis with ${koalas.toFixed(2)} points`
+//     : koalas < dolphins && dolphins >= score
+//       ? `Dolphins wins koalas with ${dolphins.toFixed(2)} points `
+//       : koalas === dolphins && koalas >= score && dolphins >= score
+//         ? `It's a draw between Koalas and Dolphins`
+//         : `No team wins the trophy`;
 
+// console.log(calcAverage);
